fb82646f846ecbba34af1ae7703315cf4ccabbf7
SweetRomeo <berkesertel4@gmail.com>
2025-10-05 00:05:16 +0300

    NumberUtil class has been added

---
+++ numberutil.cpp
@@ -0,0 +1,60 @@
+#include "numberutil.h"
+
+NumberUtil::NumberUtil(QObject *parent)
+    : QObject{parent}
+{}
+
+int NumberUtil::sumDigits(int val)
+{
+    int sum = 0;
+    while (val)
+    {
+        sum += val % 10;
+        val /= 10;
+    }
+    return abs(sum);
+}
+
+int NumberUtil::countDigits(int val)
+{
+    int count = 0;
+    do
+    {
+        count++;
+        val /= 10;
+    } while (val);
+    return count;
+}
+
+bool NumberUtil::isArmstrong(int val)
+{
+    int temp = val;
+    int sum = 0;
+    int count = countDigits(val);
+    while (val)
+    {
+        sum += pow(val % 10, count);
+        val /= 10;
+    }
+    return temp == sum;
+}
+
+bool NumberUtil::isPrime(const int val)
+{
+    if (val <= 1)
+        return false;
+    if (val % 2 == 0)
+        return val == 2;
+    if (val % 3 == 0)
+        return val == 3;
+    if (val % 5 == 0)
+        return val == 5;
+    if (val % 7 == 0)
+        return val == 7;
+    for (int i = 11; i * i < val; i += 2)
+    {
+        if (val % i == 0)
+            return false;
+    }
+    return true;
+}
---
+++ numberutil.h
@@ -0,0 +1,20 @@
+#ifndef NUMBERUTIL_H
+#define NUMBERUTIL_H
+
+#include <QObject>
+#include <QtSql/QSqlDriverCreator>
+#include <cmath>
+
+class NumberUtil : public QObject
+{
+    Q_OBJECT
+public:
+    explicit NumberUtil(QObject *parent = nullptr);
+    int sumDigits(int val);
+    int countDigits(int val);
+    bool isArmstrong(int val);
+    bool isPrime(const int val);
+signals:
+};
+
+#endif // NUMBERUTIL_H
